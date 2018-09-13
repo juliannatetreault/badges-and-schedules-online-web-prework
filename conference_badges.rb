279b1105a2664e79b3258e27283ca0339621eb33
Julianna Tetreault <juliannatetreault@icloud.com>
2018-09-13 08:43:29 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,16 +11,6 @@ end
 return badges
 end
 
-#def assign_rooms(array)
-  #nuarray = []
-  #counter = 1
-  #array.each do |name|
-    #nuarray.push("Hello, #{name}! You'll be assigned to room #{counter}!")
-    #counter += 1
-  #end
-  #return nuarray
-#end
-
 def assign_rooms(array)
   room_assignments = []
   room = 1
